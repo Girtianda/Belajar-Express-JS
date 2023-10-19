7103fc539429b2d675c27d6615cb7e17eb7bb772
Tin Suhartini <girtiandalubis@gmail.com>
2023-10-19 10:39:26 +0700

    ajnafefnafnasidfai

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 5111
+
+app.get("/Girr/:id", (req, res) => {
+    res.send("Pengen makan");
+});
+app.post("/Yanto/:id", (req, res) => {
+    res.send("Nasi padang best");
+});
+app.push("/Dadang/:id", (req, res) => {
+    res.send("Jualan sayur dimana?");
+});
+app.delete("/Agus/:id", (req, res) => {
+    res.send("Hallo ini Agus");
+});
+app.listen(port,() => console.log(`Server running on port ${port}`))
